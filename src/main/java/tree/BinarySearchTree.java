c123c1a976c762ef7150141887c9256a841e4299
prowidelabs <p.plcp2@prowidelabs.com>
2017-09-21 11:34:39 +0700

    full BST implementation & test

--- src/main/java/tree/BinarySearchTree.java
+++ src/main/java/tree/BinarySearchTree.java
@@ -1,5 +1,9 @@
 package tree;
 
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class BinarySearchTree<E extends Comparable<E>> {
     class TreeNode<V extends Comparable<V>> {
         V value;
@@ -14,14 +18,14 @@ public class BinarySearchTree<E extends Comparable<E>> {
             this.right = null;
         }
     }
-    private TreeNode<E> root;
+    private TreeNode<E> minusInfinityNode;
 
     public BinarySearchTree() {
+        minusInfinityNode = new TreeNode<>(null, null);
     }
 
-
     public boolean search(E element) {
-        TreeNode<E> curr = root;
+        TreeNode<E> curr = minusInfinityNode.right;
         while (curr != null) {
             int compareResult = curr.value.compareTo(element);
             if (compareResult == 0)
@@ -36,11 +40,11 @@ public class BinarySearchTree<E extends Comparable<E>> {
     }
 
     public BinarySearchTree<E> add(E element) {
-        if (root == null) {
-            root = new TreeNode<>(element, null);
+        if (minusInfinityNode.right == null) {
+            minusInfinityNode.right = new TreeNode<>(element, minusInfinityNode);
             return this;
         }
-        TreeNode<E> curr = root;
+        TreeNode<E> curr = minusInfinityNode.right;
         while (true) {
             int compareResult = curr.value.compareTo(element);
             if (compareResult == 0) {
@@ -64,7 +68,7 @@ public class BinarySearchTree<E extends Comparable<E>> {
     }
 
     public boolean remove(E element) {
-        TreeNode<E> curr = root;
+        TreeNode<E> curr = minusInfinityNode.right;
         while (curr != null) {
             int compareResult = curr.value.compareTo(element);
             if (compareResult == 0) {
@@ -80,13 +84,25 @@ public class BinarySearchTree<E extends Comparable<E>> {
     }
 
     private void remove(TreeNode<E> node) {
-        if (node.left == null && node.right == null)
+        if (node.left == null && node.right == null) {
+            if (node.parent.left == node)
+                node.parent.left = null;
+            else
+                node.parent.right = null;
             node.parent = null;
-        else if (node.left == null)
-            node.parent.right = node.right;
-        else if (node.right == null)
-            node.parent.left = node.left;
-        else {
+        } else if (node.left == null) {
+            if (node.parent.left == node)
+                node.parent.left = node.right;
+            else
+                node.parent.right = node.right;
+            node.parent = null;
+        } else if (node.right == null) {
+            if (node.parent.left == node)
+                node.parent.left = node.left;
+            else
+                node.parent.right = node.left;
+            node.parent = null;
+        } else {
             TreeNode<E> toRemove = findMin(node.right);
             node.value = toRemove.value;
             remove(toRemove);
@@ -101,4 +117,71 @@ public class BinarySearchTree<E extends Comparable<E>> {
                 node = node.left;
         }
     }
+
+    public <R> R inOrderTraversal(Function<TreeNode<E>, R> visitor, R zero, BinaryOperator<R> reducer, Function<R, Boolean> terminator) {
+        if (minusInfinityNode.right == null)
+            return zero;
+
+        return reducer.apply(inOrderTraversal(minusInfinityNode.right, visitor, reducer, terminator), zero);
+    }
+
+    private <R> R inOrderTraversal(TreeNode<E> root, Function<TreeNode<E>, R> visitor, BinaryOperator<R> reducer, Function<R, Boolean> terminator) {
+        R res = visitor.apply(root);
+
+        if (res != null && terminator.apply(res))
+            return res;
+
+        if (root.left != null)
+            res = res != null ?
+                    reducer.apply(res, inOrderTraversal(root.left, visitor, reducer, terminator)):
+                    inOrderTraversal(root.left, visitor, reducer, terminator);
+
+        if (res != null && terminator.apply(res))
+            return res;
+
+        if (root.right != null)
+            res = res != null ?
+                    reducer.apply(res, inOrderTraversal(root.right, visitor, reducer, terminator)):
+                    inOrderTraversal(root.right, visitor, reducer, terminator);
+
+        return res;
+    }
+
+    public <R> R preOrderTraversal(Function<TreeNode<E>, R> visitor, R zero, BinaryOperator<R> reducer, Function<R, Boolean> terminator) {
+        if (minusInfinityNode.right == null)
+            return zero;
+
+        return preOrderTraversal(minusInfinityNode.right, visitor, reducer, terminator);
+    }
+
+    private <R> R preOrderTraversal(TreeNode<E> root, Function<TreeNode<E>, R> visitor, BinaryOperator<R> reducer, Function<R, Boolean> terminator) {
+        R res = null;
+
+        if (root.left != null)
+            res = preOrderTraversal(root.left, visitor, reducer, terminator);
+        if (res != null && terminator.apply(res))
+            return res;
+
+        res = res != null ?
+                reducer.apply(res, visitor.apply(root)) :
+                visitor.apply(root);
+        if (res != null && terminator.apply(res))
+            return res;
+
+        if (root.right != null)
+            res = res != null ?
+                    reducer.apply(res, preOrderTraversal(root.right, visitor, reducer, terminator)) :
+                    preOrderTraversal(root.right, visitor, reducer, terminator);
+
+        return res;
+    }
+
+    public boolean all(Predicate<E> predicate) {
+        return preOrderTraversal(n -> predicate.test(n.value),true, (a, b) -> a && b, b -> !b);
+    }
+
+    public boolean any(Predicate<E> predicate) {
+        return preOrderTraversal(n -> predicate.test(n.value),false, (a, b) -> a || b, b -> b);
+    }
+
 }
--- src/test/java/tree/BinarySearchTreeTest.java
+++ src/test/java/tree/BinarySearchTreeTest.java
@@ -7,7 +7,16 @@ public class BinarySearchTreeTest {
     @Test
     public void testAdd() {
         BinarySearchTree<Integer> tree = new BinarySearchTree<>();
-        tree.add(10).add(5).add(7).add(1);
+        tree.add(10).add(5).add(7).add(8).add(1).add(5);
+        Assert.assertTrue(isOrdered(tree));
+    }
+
+    @Test
+    public void testSearch() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        Assert.assertFalse("search in empty tree", tree.search(3));
+
+        tree.add(10).add(5).add(7).add(8).add(1).add(5);
         Assert.assertTrue("search root elem", tree.search(10));
         Assert.assertTrue("search leaf elem", tree.search(7));
         Assert.assertTrue("search internal elem", tree.search(5));
@@ -18,10 +27,110 @@ public class BinarySearchTreeTest {
     public void testRemove() {
         BinarySearchTree<Integer> tree = new BinarySearchTree<>();
         tree.add(10).add(5).add(7).add(1);
-        Assert.assertTrue("remove root elem", tree.remove(10));
         Assert.assertTrue("remove leaf elem", tree.remove(7));
+        Assert.assertFalse("search removed leaf elem", tree.search(7));
+        Assert.assertTrue(isOrdered(tree));
+
+        tree = new BinarySearchTree<>();
+        tree.add(10).add(5).add(7).add(1);
         Assert.assertTrue("remove internal elem", tree.remove(5));
+        Assert.assertFalse("search removed internal elem", tree.search(5));
+        Assert.assertTrue(isOrdered(tree));
+
+        tree = new BinarySearchTree<>();
+        tree.add(10).add(5).add(7).add(1).add(6);
+        Assert.assertTrue("remove internal elem with big tree on the right", tree.remove(5));
+        Assert.assertFalse("search removed internal elem with big tree on the right", tree.search(5));
+        Assert.assertTrue(isOrdered(tree));
+
+        tree = new BinarySearchTree<>();
+        tree.add(10).add(5).add(7).add(1);
         Assert.assertFalse("remove absent elem", tree.remove(3));
+        Assert.assertTrue(isOrdered(tree));
+
+        tree = new BinarySearchTree<>();
+        tree.add(10).add(5).add(7).add(1).add(2);
+        Assert.assertTrue("remove internal elem with right child", tree.remove(1));
+        Assert.assertFalse("search removed internal elem with right child", tree.search(1));
+        Assert.assertTrue(isOrdered(tree));
+
+        tree = new BinarySearchTree<>();
+        tree.add(10).add(5).add(11).add(1);
+        Assert.assertTrue("remove internal elem with right child", tree.remove(5));
+        Assert.assertFalse("search removed internal elem with right child", tree.search(5));
+        Assert.assertTrue(isOrdered(tree));
+
+        tree = new BinarySearchTree<>();
+        tree.add(10).add(11).add(12).add(1).add(2);
+        Assert.assertTrue("remove internal elem with left child", tree.remove(11));
+        Assert.assertFalse("search removed internal elem with left child", tree.search(11));
+        Assert.assertTrue(isOrdered(tree));
+
+        tree = new BinarySearchTree<>();
+        tree.add(10).add(5).add(7).add(1);
+        Assert.assertTrue("remove root elem with one child", tree.remove(10));
+        Assert.assertFalse("search removed root elem with one child", tree.search(10));
+        Assert.assertTrue(isOrdered(tree));
+
+        tree = new BinarySearchTree<>();
+        tree.add(10).add(15).add(5).add(7).add(1);
+        Assert.assertTrue("remove root elem with two children", tree.remove(10));
+        Assert.assertFalse("search removed root elem with two children", tree.search(10));
+        Assert.assertTrue(isOrdered(tree));
+
+        tree = new BinarySearchTree<>();
+        tree.add(10);
+        Assert.assertTrue("remove root elem with no children", tree.remove(10));
+        Assert.assertFalse("search removed root elem with no children", tree.search(10));
+
+        tree = new BinarySearchTree<>();
+        Assert.assertFalse("remove from empty tree", tree.remove(10));
     }
 
+    @Test
+    public void testInOrderTraversal() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        Assert.assertTrue("in-order traversal on empty tree", isOrdered(tree));
+
+        tree.add(10).add(5).add(7).add(1).add(15).add(45);
+        Assert.assertTrue(isOrdered(tree));
+    }
+
+    @Test
+    public void testPreOrderTraversal() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        tree.add(10).add(5).add(7).add(1).add(15).add(45);
+        Assert.assertTrue(isOrdered(tree));
+        Assert.assertEquals("1, 5, 7, 10, 15, 45",
+                tree.preOrderTraversal(n -> n.value.toString(), "", (a, b) -> a + ", " + b, b -> false));
+
+        tree = new BinarySearchTree<>();
+        tree.add(10).add(5).add(7).add(-1).add(15).add(45);
+        Assert.assertFalse("all > 0",
+                tree.preOrderTraversal(n -> n.value > 0, true, (a, b) -> a && b, b -> false));
+
+        tree = new BinarySearchTree<>();
+        tree.add(10).add(5).add(7).add(-1).add(15).add(45);
+        Assert.assertEquals("sum of all", (Integer)81,
+                tree.preOrderTraversal(n -> n.value, 0, (a, b) -> a + b, b -> false));
+    }
+
+    @Test
+    public void testAllAny() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+
+        tree.add(10).add(5).add(7).add(1).add(15).add(45);
+        Assert.assertTrue(tree.all(v -> v > 0));
+        Assert.assertTrue(tree.any(v -> v > 15));
+        Assert.assertFalse(tree.all(v -> v > 15));
+        Assert.assertFalse(tree.any(v -> v < 0));
+    }
+
+    private <E extends Comparable<E>> boolean isOrdered(BinarySearchTree<E> tree) {
+        return tree.inOrderTraversal(n -> n.left == null  || n.value.compareTo(n.left.value) > 0 &&
+                                          n.right == null || n.value.compareTo(n.right.value) < 0,
+                                     true,
+                                     (a, b) -> a && b,
+                                     b -> !b);
+    }
 }
